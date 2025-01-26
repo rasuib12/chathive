2502a8c5d3863bf61a7fd3d9f9ea52f59c01a7f1
Prathamesh Sharma <prathameshsharma27@gmail.com>
2025-01-26 10:36:35 +0530

    Implemented light and dark mode for the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({subsets: ["latin"]});
 
@@ -17,11 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${font.className} antialiased`}
+        className={cn(`${font.className} antialiased` , "bg-white dark:bg-[#313338]")}
       >
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem={false}
+        storageKey="chathive-theme">
         {children}
+        </ThemeProvider>
       </body>
     </html>
     </ClerkProvider>
